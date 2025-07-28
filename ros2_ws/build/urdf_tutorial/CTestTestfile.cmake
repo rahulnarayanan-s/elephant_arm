76e32d361f0781810dcbea87820a1730861b9d40
nano <s.rahulnarayanan@gmail.com>
2025-07-28 22:11:09 +0530

    project work

---
+++ ros2_ws/build/urdf_tutorial/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nano/project/ros2_ws/src/robot_model_tutorials
+# Build directory: /home/nano/project/ros2_ws/build/urdf_tutorial
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
